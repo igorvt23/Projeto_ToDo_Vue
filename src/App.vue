636adfd73ad356cdfd2ac2c75301621a8e15a59e
Igor <igorvianat@gmail.com>
2026-04-17 21:24:25 -0300

    second commit

---
+++ src/App.vue
@@ -0,0 +1,167 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="container">
+
+    <div id="containerFilho">
+      <TheHeader/>
+      <div :id="idDivAdd">
+        <input type="text" :id="idInput" value="" @keyup.enter="addNewTasks">
+        <span @click="addNewTasks" :id="idDivBotao">+</span>
+      </div>
+      <div v-if="registros.length > 0" id="divRegistros">
+        <div v-for="item in registros" >
+          <div :class="classTable">
+              <span @click="checkToDo(item.id)" :id="`registro_${item.id}`" :class="['check', (item.checked) ? 'checked' : 'unchecked']">✓</span>
+              <input type="text" :value="item.texto" placeholder="Digite suas tarefas">
+              <span @click="removeTask(item.id)" :class="classRemoveButton">-</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TheHeader from "./components/TheHeader.vue";
+
+  export default{
+    name: 'app',
+    components:{
+      TheHeader,
+    },
+    data() {
+      return {
+        idx: 1,
+        registros: [],
+
+        // IDS
+        idInput: 'idNovoCampo',
+        idDivAdd: 'idDivAdd',
+        idDivBotao: 'idDivBotao',
+        classTable: 'classTable',
+        classRemoveButton: 'classRemoveButton',
+
+      }
+      
+    },
+    methods:{
+        addNewTasks(){
+
+          const campo = document.getElementById(this.idInput);
+          if(campo.value.trim() != ""){
+            this.registros.push({"id": this.idx++, "texto": campo.value, checked: false });
+            campo.value = '';
+          }
+          console.log('chegou aqui');
+
+        },
+        checkToDo(id){
+            const checkToDo = this.registros.find(i => i.id === id);
+            const registro = document.getElementById(`registro_${id}`);
+            if(registro.classList.contains('checked')){
+              checkToDo.checked = false;              
+            } else {
+              checkToDo.checked = true;
+            }
+        },
+
+        removeTask(id){
+          const registro = document.getElementById(`registro_${id}`);
+          registro.parentElement.remove();
+
+        }
+    }
+  }
+</script>
+
+<style scoped></style>
+<style>
+
+
+  #container{
+    display: flex;
+    justify-content: center;
+    margin-top: 20%;
+  }
+
+  #idDivAdd{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 30px,
+  }  
+  #idDivBotao {
+    background-color: #59c765;
+    border-radius: 5px;
+    margin-left: 1rem;
+    height: 1.5rem;
+    width: 1.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 800;
+    color: #fff;
+    font-size: 25px;
+    cursor: pointer
+  }
+
+  .classTable{
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px,
+
+  }
+  .checked{
+    background-color: #59c765;
+
+  }
+  .unchecked{
+    background-color: #cce5af;
+
+  }
+  .unchecked:hover{
+    background-color: #59c765;
+
+  }
+  .check{
+    cursor:pointer;
+    margin-right: 1rem;
+    border-radius: 5px;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    height: 1.5rem;
+    width: 1.5rem;
+    color: #fff;
+
+  } 
+  .classRemoveButton{
+    cursor: pointer;
+    margin-left: 1rem;
+    height: 1.5rem;
+    width: 1.5rem;
+    border-radius: 5px;
+    color: #fff;
+    font-weight: 700;
+    font-size: 25px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #FF2C2C;
+  }
+
+  #idNovoCampo{
+    margin-left: 2.5rem;
+  }
+
+  #divRegistros{
+    margin-top: 1rem;
+    max-height: 200px;
+    overflow: auto;
+
+  }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Olá vivente!</h1>
+    <span>Adicione as suas tarefas</span>
+</template>
+
+<script>
+    export default{
+        name: 'TheHeader',
+    }
+</script>
+
+<style>
+    h1{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    span{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
